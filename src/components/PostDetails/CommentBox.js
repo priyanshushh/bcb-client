222cdc36419e262a7198116d661229aa8ee06866
priyanshusharmaaug03 <priyanshusharmaaug03@gmail.com>
2022-12-02 13:40:31 +0530

    ...

--- src/components/PostDetails/CommentBox.js
+++ src/components/PostDetails/CommentBox.js
@@ -1,4 +1,5 @@
-export default disqus_config = function ({ id }) {
+const disqus_config = function ({ id }) {
   this.page.url = `https://bcb-queries.netlify.app/posts/${id}`; // Replace PAGE_URL with your page's canonical URL variable
   this.page.identifier = { id }; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
+export default disqus_config;
